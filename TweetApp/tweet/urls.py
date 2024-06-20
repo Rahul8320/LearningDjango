cea6af672a6114a3261e3e3d9d9279e610cd2253
Rahul Dey <rahuldey832000@gmail.com>
2024-06-20 21:56:13 +0530

    Add Create and Update tweet

--- TweetApp/tweet/urls.py
+++ TweetApp/tweet/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", view=views.home, name="home")    
+    path("", view=views.home, name="home"),
+    path("create/", view=views.create_tweet, name="create_tweet"),
+    path("<int:tweet_id>/update/", view=views.update_tweet, name="update_tweet"),
+    path("<int:tweet_id>/delete/", view=views.delete_tweet, name="delete_tweet")    
 ]
--- TweetApp/tweet/views.py
+++ TweetApp/tweet/views.py
@@ -1,5 +1,46 @@
 from django.shortcuts import render
+from .models import Tweet
+from .forms import TweetForm
+from django.shortcuts import get_object_or_404,redirect
 
 # Create your views here.
 def home(request):
-    return render(request, 'tweet/home.html')
+    tweets = Tweet.objects.all().order_by("-created_at")
+    return render(request, 'tweet/home.html', {"tweets": tweets})
+
+def create_tweet(request):
+    if request.method == "POST":
+        form = TweetForm(request.POST, request.FILES)
+        if form.is_valid():
+            tweet = form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
+            return redirect('home')
+    else:
+        form = TweetForm()
+
+    return render(request, 'tweet/create.html', {"form": form})
+
+def update_tweet(request, tweet_id):
+    tweet = get_object_or_404(Tweet, pk = tweet_id, user = request.user)
+    
+    if request.method == "POST":
+        form = TweetForm(request.POST, request.FILES, instance=tweet)
+        if form.is_valid():
+            tweet = form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
+            return redirect('home')
+    else:
+        form = TweetForm(instance=tweet)
+
+    return render(request, 'tweet/create.html', {"form": form})
+
+def delete_tweet(request, tweet_id):
+    tweet = get_object_or_404(Tweet, pk = tweet_id, user = request.user)
+    
+    if request.method == "POST":
+        tweet.delete()
+        return redirect("home")
+    
+    return render(request, 'tweet/delete.html', {"tweet": tweet})
